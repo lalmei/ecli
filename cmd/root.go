d3688fcce488aa869a2397a926162b46e8e125fb
matm <matm@outofpluto.com>
2016-09-27 16:47:28 +0200

    Add main description message

--- cmd/root.go
+++ cmd/root.go
@@ -28,13 +28,11 @@ var cfgFile string
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "ecli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Simple client for Keen Eye Engine",
+	Long: `Ecli is a CLI client for the Keen Eye Engine. It can be used to upload,
+find a slide or get slide information.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Also it is possible no browse the work list like browsing a UNIX filesystem.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
