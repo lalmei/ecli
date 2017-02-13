5f23e805a29124fcf8669c20d0c0e4ae63d213f7
matm <mathias.monnerville@keeneyetechnologies.com>
2017-02-13 14:20:48 +0100

    Better short description

--- cmd/version.go
+++ cmd/version.go
@@ -28,7 +28,7 @@ import (
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Show engine's API version",
+	Short: "Show tool version",
 	Run: func(cmd *cobra.Command, args []string) {
 		v, err := api.Version()
 		if err != nil {
@@ -46,15 +46,4 @@ var versionCmd = &cobra.Command{
 
 func init() {
 	RootCmd.AddCommand(versionCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
 }
